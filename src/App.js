8b42c866b169a20df8cbcf311c6e91389f3ced98
KHO11 <kevhydeopoku@gmail.com>
2021-06-21 10:32:53 +0100

    memes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    );
+  }
+}
+
+export default App;
